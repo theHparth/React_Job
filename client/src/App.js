1ef92c0e76d61bbb7b424b723d345b92c508574b
theHparth <pphirpara@gmail.com>
2022-03-04 13:37:03 -0500

    if user login then redirect to dashboard otherwise on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Error, Register, Landing } from "./pages";
+import { Error, Register, Landing, ProtectedRoute } from "./pages";
 import {
   AddJob,
   AllJobs,
@@ -11,7 +11,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />}></Route>
           <Route path="add-job" element={<AddJob />}></Route>
