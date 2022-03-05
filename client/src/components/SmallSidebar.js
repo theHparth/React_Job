cc99c8f00b327365e3bf3f16bc2c872156753a7d
theHparth <pphirpara@gmail.com>
2022-03-05 11:03:05 -0500

    small side bar set fix with differne tsize of page

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,12 +1,31 @@
-import React from "react";
 import Wrapper from "../asserts/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
 
-const SmallSidebar = () => {
+import Logo from "./Logo";
+import NavLinks from "./Navlinks";
+
+export const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
+
   return (
     <Wrapper>
-      <h4>samll side bar</h4>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
-
 export default SmallSidebar;
