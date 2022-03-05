70902d6edf96f5fe06422bf785564a8c3a24ba2f
theHparth <pphirpara@gmail.com>
2022-03-05 12:03:49 -0500

    auth user in postman setup and in serverside is done

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,21 @@
+import jwt from "jsonwebtoken";
+import { UnAuthenticatedError } from "../errors/index.js";
 const auth = async (req, res, next) => {
-  const headers = req.headers;
+  // check header
   const authHeader = req.headers.authorization;
-  console.log(headers);
-  console.log(authHeader);
-  next();
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnAuthenticatedError("Authentication invalid");
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(payload)
+    // attach the user request object
+    // req.user = payload
+    req.user = { userId: payload.userId };
+    next();
+  } catch (error) {
+    throw new UnAuthenticatedError("Authentication invalid");
+  }
 };
-
 export default auth;
