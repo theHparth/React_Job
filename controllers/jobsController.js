07e36db49faaa1caf0b1733582553eba2429a8fa
theHparth <pphirpara@gmail.com>
2022-03-11 02:40:48 -0500

    f

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -5,6 +5,7 @@ import checkPermissions from "../utils/checkPermissions.js";
 
 // stats for
 import mongoose from "mongoose";
+import moment from "moment";
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -84,7 +85,42 @@ const showStats = async (req, res) => {
     interview: stats.interview || 0,
     declined: stats.declined || 0,
   };
-  let monthlyApplications = [];
+  // for bar chart monthly application
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: {
+          year: {
+            $year: "$createdAt",
+          },
+          month: {
+            $month: "$createdAt",
+          },
+        },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+
+  // to srot and arrange proper by date
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      // accepts 0-11
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
