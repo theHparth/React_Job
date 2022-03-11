e3ef299f16492275091fa521d5384f1dc5229f63
theHparth <pphirpara@gmail.com>
2022-03-11 03:03:21 -0500

    chart finish with error

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+import { useAppContext } from "../context/appContext";
+import Wrapper from "../asserts/wrappers/ChartsContainer";
 
 const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "AreaChart" : "BarChart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
