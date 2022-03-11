96607e2a7d8342f6a790c61ccaea4bd0f56515cc
theHparth <pphirpara@gmail.com>
2022-03-11 03:53:38 -0500

    page limit in backend

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -47,12 +47,25 @@ const getAllJobs = async (req, res) => {
   if (sort === "z-a") {
     result = result.sort("-position");
   }
+
+  // skip(3)  means skip first 3-three value
+
+  // setup pagination
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const totalJobs = await Job.countDocuments(queryObject);
+
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
+/////
 const updateJob = async (req, res) => {
   const { id: jobId } = req.params;
 
